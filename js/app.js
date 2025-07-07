e6ab3f5dfa1847b135faa2554618e9049eb34386
REstesna <mohammad.allahyari.am@gmail.com>
2025-07-07 21:13:36 +0330

    1st

---
+++ js/app.js
@@ -0,0 +1,54 @@
+const $ = document;
+
+// selectors
+
+const blockQuoteElem = $.querySelector("blockquote");
+const authorSpanElem = $.querySelector("#author");
+const newQuoteBtn = $.querySelector("#new-quote");
+const quoteContainerElem = $.querySelector(".quote-container");
+
+
+
+// generate new quote
+
+async function generateNewQuote() {
+  // quoteContainerElem.classList.add('load-animaition')
+
+  // quoteContainerElem.addEventListener('animationend', () => {
+  //     quoteContainerElem.classList.remove('load-animaition')
+  // })
+
+  document.documentElement.style.setProperty("--ani-start-value", "50%");
+  let mainQuote = await getQuoteFromApi();
+  authorSpanElem.textContent = `- ${mainQuote.author}`;
+  blockQuoteElem.textContent = mainQuote.quote;
+  document.documentElement.style.setProperty("--ani-start-value", "20%");
+    newQuoteBtn.disabled = false
+
+}
+
+// fetch new quote from api
+
+async function getQuoteFromApi() {
+
+    newQuoteBtn.disabled = true;
+    await new Promise(requestAnimationFrame);
+  try {
+    let quotePromise = await fetch("https://quotes-api-self.vercel.app/quote");
+    let data = await quotePromise.json();
+
+    return data;
+  } catch {
+    alert('Check your internet and try again');
+
+    return {
+        "author": " :/",
+        "quote": "Check your internet and try again"
+    };
+  }
+
+
+}
+
+
+newQuoteBtn.addEventListener("click", generateNewQuote);
